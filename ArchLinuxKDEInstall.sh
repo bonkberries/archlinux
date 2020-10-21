84d1aad1e808a51c691a23c167f91281f607050e
bonkberries <65596971+bonkberries@users.noreply.github.com>
2020-10-21 16:04:47 +0300

    ArchLinux KDE Installer
    does what it says

---
+++ ArchLinuxKDEInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing KDE and other programs."
+
+sudo pacman -S plasma-desktop pulseaudio plasma-pa plasma-nm dolphin konsole ark chromium kate kdenlive lightdm lightdm-gtk-greeter -y
+
+sudo systemctl enable lightdm
+
+echo "Installed."
+
+echo "Rebooting in 5 seconds."
+sleep 5
+sudo reboot
